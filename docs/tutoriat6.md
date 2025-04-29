22e050e8462825d9e24a1cdb6e334f6c3139a70b
Darius-Ștefan Țacu <88737250+stefanFortza@users.noreply.github.com>
2025-04-29 22:00:06 +0300

    Feat: Add tutorial on RegEx to AFN-λ with exercises and automata illustrations

---
+++ docs/tutoriat6.md
@@ -0,0 +1,74 @@
+--- 
+sidebar_position: 7 
+---
+
+# RegEx
+
+## Transformarea RegEx → AFN-λ
+
+### Caz i) RegEx = ∅ = λ = a, unde a ∈ Σ
+
+$$
+Limbaj ( L = \emptyset ) sau ( L = {\lambda} ) sau ( L = {a} )
+$$
+
+**Automat Finit:**
+
+![Caz i automaton](../static/img/tutoriat6/caz0.png)
+
+### Caz ii) (reuniune) $RegEx = e_1 + e_2$
+
+$$
+Limbaj ( L(e) = L(e_1) \cup L(e_2) )
+$$
+
+**Automat Finit:**
+
+![Caz ii automaton](../static/img/tutoriat6/caz1.png)
+
+### Caz iii) (concatenare) $RegEx = e_1 \cdot e_2$
+
+$$
+Limbaj ( L(e) = L(e_1) \cdot L(e_2) )
+$$
+
+**Automat Finit:**
+
+![Caz iii automaton](../static/img/tutoriat6/caz2.png)
+
+
+### Caz iv) (stelare) $RegEx = (e_1)^*$
+
+$$
+Limbaj \left( L(e) = (L(e_1))^* \right )
+$$
+
+**Automat Finit:**
+
+![Caz iv automaton](../static/img/tutoriat6/caz3.png)
+
+## Exerciții
+
+1. Folosind algoritmul de la curs/seminar, transformați următorul automat finit într-o expresie regulată echivalentă (faceți și desenele și pentru pașii intermediari).
+  ![Fotografie DFA](../static/img/tutoriat6/ex1.png)
+
+2. Se dă următoarea expresie regulată:
+
+  $$
+  (e_1 + e_2) \cdot (e_3)^*, \quad \text{unde}
+  $$
+
+  $$
+  e_1 = a^* \cdot bb, \quad e_2 = c \cdot (dd)^* \cdot e, \quad e_3 = aa \cdot (b + ccc)
+  $$
+
+Să se obțină un automat finit care acceptă același limbaj ca cel descris de expresia dată.
+
+3. Se dă următoarea expresie regulată:
+
+$$
+aba^*aba(ba)^*a
+$$
+
+Să se obțină un automat finit care acceptă același limbaj ca cel descris de expresia dată.
+
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,162 +7,162 @@ import rehypeKatex from "rehype-katex";
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: "Tutoriat LFA",
-  tagline: "Materiale pentru cursul de LFA",
-  // favicon: "img/favicon.ico",
-  favicon: "img/thumbnail.png",
+	title: "Tutoriat LFA",
+	tagline: "Materiale pentru cursul de LFA",
+	// favicon: "img/favicon.ico",
+	favicon: "img/thumbnail.png",
 
-  // Set the production url of your site here
-  // url: 'https://TutoriatLFA.github.io',
-  // url: 'https://fmi-limbaje-formale-si-automate.github.io',
-  url: "https://fmi-limbaje-formale-si-automate.netlify.app", // Url to your site with no trailing slash
-  baseUrl: "/", // Base directory of your site relative to your repo
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  // baseUrl: '/TutoriatLFA/',
+	// Set the production url of your site here
+	// url: 'https://TutoriatLFA.github.io',
+	// url: 'https://fmi-limbaje-formale-si-automate.github.io',
+	url: "https://fmi-limbaje-formale-si-automate.netlify.app", // Url to your site with no trailing slash
+	baseUrl: "/", // Base directory of your site relative to your repo
+	// Set the /<baseUrl>/ pathname under which your site is served
+	// For GitHub pages deployment, it is often '/<projectName>/'
+	// baseUrl: '/TutoriatLFA/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "TutoriatLFA", // Usually your GitHub org/user name.
-  projectName: "TutoriatLFA", // Usually your repo name.
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	organizationName: "TutoriatLFA", // Usually your GitHub org/user name.
+	projectName: "TutoriatLFA", // Usually your repo name.
 
-  trailingSlash: false,
-  deploymentBranch: "gh-pages",
+	trailingSlash: false,
+	deploymentBranch: "gh-pages",
 
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
+	onBrokenLinks: "throw",
+	onBrokenMarkdownLinks: "warn",
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
-  },
+	// Even if you don't use internationalization, you can use this field to set
+	// useful metadata like html lang. For example, if your site is Chinese, you
+	// may want to replace "en" with "zh-Hans".
+	i18n: {
+		defaultLocale: "en",
+		locales: ["en"],
+	},
 
-  presets: [
-    [
-      "classic",
-      {
-        docs: {
-          sidebarPath: "./sidebars.ts",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-          remarkPlugins: [remarkMath],
-          rehypePlugins: [rehypeKatex],
-        },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ["rss", "atom"],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-          // Useful options to enforce blogging best practices
-          onInlineTags: "warn",
-          onInlineAuthors: "warn",
-          onUntruncatedBlogPosts: "warn",
-        },
-        theme: {
-          customCss: "./src/css/custom.css",
-        },
-        gtag: {
-          trackingID: "G-QB03VNBW9Q",
-          anonymizeIP: true,
-        },
-      } satisfies Preset.Options,
-    ],
-  ],
+	presets: [
+		[
+			"classic",
+			{
+				docs: {
+					sidebarPath: "./sidebars.ts",
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl:
+						"https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+					remarkPlugins: [remarkMath],
+					rehypePlugins: [rehypeKatex],
+				},
+				blog: {
+					showReadingTime: true,
+					feedOptions: {
+						type: ["rss", "atom"],
+						xslt: true,
+					},
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl:
+						"https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+					// Useful options to enforce blogging best practices
+					onInlineTags: "warn",
+					onInlineAuthors: "warn",
+					onUntruncatedBlogPosts: "warn",
+				},
+				theme: {
+					customCss: "./src/css/custom.css",
+				},
+				gtag: {
+					trackingID: "G-QB03VNBW9Q",
+					anonymizeIP: true,
+				},
+			} satisfies Preset.Options,
+		],
+	],
 
-  themeConfig: {
-    // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
-    navbar: {
-      title: "Tutoriat LFA",
-      logo: {
-        alt: "My Site Logo",
-        // src: "img/logo.svg",
-        src: "img/thumbnail.png",
-        srcDark: "img/logo_dark.png",
-      },
-      items: [
-        {
-          type: "docSidebar",
-          sidebarId: "tutorialSidebar",
-          position: "left",
-          label: "Tutorial",
-        },
-        // { to: '/blog', label: 'Blog', position: 'left' },
-        // {
-        //   href: 'https://github.com/TutoriatLFA/TutoriatLFA',
-        //   label: 'GitHub',
-        //   position: 'right',
-        // },
-      ],
-    },
-    footer: {
-      style: "dark",
-      links: [
-        {
-          title: "Docs",
-          items: [
-            {
-              label: "Tutorial",
-              to: "/docs/intro",
-            },
-          ],
-        },
-        {
-          title: "Community",
-          items: [
-            // {
-            //   label: 'Stack Overflow',
-            //   href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            // },
-            {
-              label: "Whatsapp",
-              href: "https://tinyurl.com/tutoriatLFA-2025",
-            },
-          ],
-        },
-        {
-          title: "More",
-          items: [
-            /*     {
+	themeConfig: {
+		// Replace with your project's social card
+		image: "img/docusaurus-social-card.jpg",
+		navbar: {
+			title: "Tutoriat LFA",
+			logo: {
+				alt: "My Site Logo",
+				// src: "img/logo.svg",
+				src: "img/thumbnail.png",
+				srcDark: "img/logo_dark.png",
+			},
+			items: [
+				{
+					type: "docSidebar",
+					sidebarId: "tutorialSidebar",
+					position: "left",
+					label: "Tutoriat",
+				},
+				// { to: '/blog', label: 'Blog', position: 'left' },
+				// {
+				//   href: 'https://github.com/TutoriatLFA/TutoriatLFA',
+				//   label: 'GitHub',
+				//   position: 'right',
+				// },
+			],
+		},
+		footer: {
+			style: "dark",
+			links: [
+				{
+					title: "Docs",
+					items: [
+						{
+							label: "Tutorial",
+							to: "/docs/intro",
+						},
+					],
+				},
+				{
+					title: "Community",
+					items: [
+						// {
+						//   label: 'Stack Overflow',
+						//   href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+						// },
+						{
+							label: "Whatsapp",
+							href: "https://tinyurl.com/tutoriatLFA-2025",
+						},
+					],
+				},
+				{
+					title: "More",
+					items: [
+						/*     {
                   label: 'Blog',
                   to: '/blog',
                 }, */
-            /*             {
+						/*             {
                           label: 'GitHub',
                           href: 'https://github.com/TutoriatLFA/TutoriatLFA',
                         }, */
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Free for non-commercial use .`,
-    },
-    prism: {
-      // theme: prismThemes.github,
-      theme: prismThemes.github,
-      // darkTheme: prismThemes.dracula,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
+					],
+				},
+			],
+			copyright: `Copyright © ${new Date().getFullYear()} Free for non-commercial use .`,
+		},
+		prism: {
+			// theme: prismThemes.github,
+			theme: prismThemes.github,
+			// darkTheme: prismThemes.dracula,
+			darkTheme: prismThemes.dracula,
+		},
+	} satisfies Preset.ThemeConfig,
 
-  stylesheets: [
-    {
-      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
-      type: "text/css",
-      integrity:
-        "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
-      crossorigin: "anonymous",
-    },
-  ],
+	stylesheets: [
+		{
+			href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+			type: "text/css",
+			integrity:
+				"sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+			crossorigin: "anonymous",
+		},
+	],
 };
 
 export default config;
